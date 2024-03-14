5ecac4231dda1b8cd1506dd0b1294ea9f20efc7f
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-14 21:21:34 +0100

    commit clases modelo terminadas

---
+++ src/Modelo/Escudo.java
@@ -0,0 +1,39 @@
+package Modelo;
+
+public class Escudo {
+
+	private int idEscudo;
+	private int defensa;
+	private int durabilidad;
+	private String material;
+	public int getIdEscudo() {
+		return idEscudo;
+	}
+	public void setIdEscudo(int idEscudo) {
+		this.idEscudo = idEscudo;
+	}
+	public int getDefensa() {
+		return defensa;
+	}
+	public void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+	public int getDurabilidad() {
+		return durabilidad;
+	}
+	public void setDurabilidad(int durabilidad) {
+		this.durabilidad = durabilidad;
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	@Override
+	public String toString() {
+		return "Escudo [idEscudo=" + idEscudo + ", defensa=" + defensa + ", durabilidad=" + durabilidad + ", material="
+				+ material + "]";
+	}
+	
+}
