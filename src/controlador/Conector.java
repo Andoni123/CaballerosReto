eba2c4c7f8f9241d97c118574d02ddc33ca2fc20
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-14 16:24:23 +0100

    commit conector y gestorBBDD

---
+++ src/controlador/Conector.java
@@ -0,0 +1,34 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector {
+	protected Connection con;
+
+	private String host = "localhost";
+	private String bbdd = "caballeros";
+	private String usuario = "root";
+	private String contrasenia = "";
+	
+	public void conectar() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			this.con = DriverManager.getConnection("jdbc:mysql://" + host + "/" + bbdd, usuario, contrasenia);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void cerrar() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
