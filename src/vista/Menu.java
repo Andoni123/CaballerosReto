c6fe6fe2055a88dc2717a3c7b59754735c463ab1
plaiaundi <plaiaundi@MININT-5OLIS4G>
2024-03-15 18:48:34 +0100

    commit ajustes menu

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -5,18 +5,22 @@ public class Menu {
 	public static final int GESTOR =1;
 	public static final int BATALLA = 2;
 	public static final int MAS_INFORMACION = 3;
+	public static final int SALIR = 4;
 	
 	public static final int GESTIONAR_CABALLEROS = 1;
 	public static final int GESTIONAR_ARMAS = 2;
 	public static final int GESTIONAR_ESCUDOS = 3;
 	public static final int GESTIONAR_ESCUDEROS = 4;
 	public static final int GESTIONAR_CABALLOS = 5;
+	public static final int SALIR_CRUD = 6;
+	
 
 	public static final int INSERTAR_CABALLERO = 1;
 	public static final int MOSTRAR_CABALLERO = 2;
 	public static final int MOSTRAR_CABALLEROS = 3;
 	public static final int MODIFICAR_CABALLERO = 4;
 	public static final int ELIMINAR_CABALLERO = 5;
+	public static final int SALIR_CABALLERO = 6;
 
 	
 
@@ -35,6 +39,7 @@ public class Menu {
 		System.out.println(GESTIONAR_ESCUDOS + "-Gestionar escudos");
 		System.out.println(GESTIONAR_ESCUDEROS + "-Gestionar escuderos");
 		System.out.println(GESTIONAR_CABALLOS + "-Gestionar caballos");
+		System.out.println(SALIR_CRUD + "Salir");
 		
 	}
 	
@@ -47,5 +52,6 @@ public class Menu {
 		System.out.println(MOSTRAR_CABALLEROS + "-Mostrar todos los caballeros");
 		System.out.println(MODIFICAR_CABALLERO + "-Modificar caballero");
 		System.out.println(ELIMINAR_CABALLERO + "-Eliminar caballero");
+		System.out.println(SALIR_CABALLERO + "Salir");
 	}
 }
